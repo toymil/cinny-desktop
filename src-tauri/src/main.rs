92bdcdef5858fb4ae1ffba741cc0b36912f408bf
SPQV MF <B90FCF15A76E432D@outlook.com>
2023-01-01 17:10:27 +0800

    Switch to 4 space indentation in `main.rs`

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,18 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 #[cfg(target_os = "macos")]
 mod menu;
 
 fn main() {
-  let builder = tauri::Builder::default();
+    let builder = tauri::Builder::default();
 
-  #[cfg(target_os = "macos")]
-  let builder = builder.menu(menu::menu());
+    #[cfg(target_os = "macos")]
+    let builder = builder.menu(menu::menu());
 
-  builder
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    builder
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
